f8fbf9d51ac6743202713ea3827faf152764c4ff
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-17 22:09:57 +0500

    Task3

---
+++ Tyuiu.FilimonchukED.Sprint4.Task3.V29.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.FilimonchukED.Sprint4.Task3.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.FilimonchukED.Sprint4.Task3.V29/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.FilimonchukED.Sprint4.Task3.V29.Lib;
+namespace Tyuiu.FilimonchukED.Sprint4.Task3.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*          *");
+            Console.WriteLine("*                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] mtrx = new int[5, 5] { { 2, 2, 3, 4, 5 },
+                                          { 1, 2, 3, 4, 5 },
+                                          { 4, 2, 3, 4, 5 },
+                                          { 7, 2, 3, 4, 5 },
+                                          { 6, 2, 3, 4, 5 }, };
+
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int cols = mtrx.Length / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение 1 столбца = " + ds.Calculate(mtrx));
+        }
+    }
+}
