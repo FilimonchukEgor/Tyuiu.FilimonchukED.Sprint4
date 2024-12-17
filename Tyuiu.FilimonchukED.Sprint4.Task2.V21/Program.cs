006e6e83bb2846b2d407a6b5195197f6a7b429a4
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-17 21:07:24 +0500

    Task2

---
+++ Tyuiu.FilimonchukED.Sprint4.Task2.V21/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.FilimonchukED.Sprint4.Task2.V21.Lib;
+namespace Tyuiu.FilimonchukED.Sprint4.Task2.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*          *");
+            Console.WriteLine("*                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите размер массива:");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                array[i] = rnd.Next(1,8);
+            }
+            Console.WriteLine("Массив: ");
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine(array[i] + "\t");
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(array));
+        }
+    }
+}
