8b6496371aae31e8bd6d5696e63f877d1d7eb109
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-18 21:09:08 +0500

    Task7

---
+++ Tyuiu.FilimonchukED.Sprint4.Task7.V25.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.FilimonchukED.Sprint4.Task7.V25.Lib;
+namespace Tyuiu.FilimonchukED.Sprint4.Task7.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int n = 4;
+            int m = 3;
+            int[,] mtrx = new int[n, m];
+            string str = "348561792486";
+            DataService ds = new DataService();
+            int res = ds.Calculate(n,m,str);
+            int wait = 38;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FilimonchukED.Sprint4.Task7.V25/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.FilimonchukED.Sprint4.Task7.V25.Lib;
+namespace Tyuiu.FilimonchukED.Sprint4.Task7.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Итоговое Решение                                                  *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*          *");
+            Console.WriteLine("*                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int rows = 4;
+            int cols = 3;
+            int[,] mtrx = new int[rows, cols];
+
+            string str = "348561792486";
+
+            int index = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            
+                
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(rows, cols, str);
+            Console.WriteLine("Сумма четных элементов массива = " + res);
+        }
+    }
+}
