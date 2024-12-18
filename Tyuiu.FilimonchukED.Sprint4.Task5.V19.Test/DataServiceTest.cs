60f32106739b0bb148058b32e1b8c305562c95fd
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-18 20:01:59 +0500

    Task5

---
+++ Tyuiu.FilimonchukED.Sprint4.Task5.V19.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.FilimonchukED.Sprint4.Task5.V19.Lib;
+namespace Tyuiu.FilimonchukED.Sprint4.Task5.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] mtrx = { { -2, 1, 2 }, { -1, 2, 3 }, { -2, 3, -2 }, };
+            int res = ds.Calculate(mtrx);
+            int wait = 5;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
